6e99e74480ce29b0da7c611dbd01387e08fe6e2b
suo235 <usukeo235@gmail.com>
2026-02-25 23:16:59 +0900

    Move MockLoginManager to test project

--- MvvmTemplate/Models/LoginManager.cs
+++ MvvmTemplate/Models/LoginManager.cs
@@ -1,39 +1,5 @@
 namespace MvvmTemplate.Models;
 
-public class MockLoginManager : ILoginManager
-{
-    public bool IsLogined
-    {
-        get => is_logined;
-        set
-        {
-            is_logined = value;
-        }
-    }
-    private bool is_logined = false;
-
-    public User? LoginUser
-    {
-        get => login_user;
-        set
-        {
-            login_user = value;
-        }
-    }
-    private User? login_user = null;
-
-    public MockLoginManager()
-    {
-        
-    }
-
-    public bool Login(string name, string password)
-    {
-        LoginUser = new User{Id = 1, Name = name, Password = password, IsAdmin = 0, IsDeleted = 0};
-        return true;
-    }
-}
-
 public class LoginManager : ILoginManager
 {
     public bool IsLogined
