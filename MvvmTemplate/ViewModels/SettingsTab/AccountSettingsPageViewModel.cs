925aa56314ff66cb490ae4b33448bb0d7b4a1f8a
suo235 <usukeo235@gmail.com>
2026-03-02 00:49:44 +0900

    Add binding targets to ViewModel

--- MvvmTemplate/ViewModels/SettingsTab/AccountSettingsPageViewModel.cs
+++ MvvmTemplate/ViewModels/SettingsTab/AccountSettingsPageViewModel.cs
@@ -1,10 +1,40 @@
 using System.ComponentModel;
 
+using MvvmTemplate.Models;
+
+using Reactive.Bindings;
+using System.Reactive.Disposables;
+using Reactive.Bindings.Extensions;
+
 namespace MvvmTemplate.ViewModels;
 
 public class AccountSettingsPageViewModel : IAccountSettingsPageViewModel
 {
+    public ReactiveProperty<string> Username { get; private set; }
+
+    public ReactiveProperty<string> Password { get; private set; }
+
+    public ReactiveProperty<string> ConfirmPassword { get; private set; }
+
 #pragma warning disable CS0067
     public event PropertyChangedEventHandler? PropertyChanged;
 #pragma warning restore CS0067
+
+    public AccountSettingsPageViewModel(ILoginManager loginManager)
+    {
+        login_manager = loginManager;
+
+        Username = new ReactiveProperty<string>().AddTo(disposable);
+        Password = new ReactiveProperty<string>().AddTo(disposable);
+        ConfirmPassword = new ReactiveProperty<string>().AddTo(disposable);
+    }
+
+    public void Dispose()
+    {
+        disposable.Dispose();
+    }
+
+    private CompositeDisposable disposable { get; } = new CompositeDisposable();
+
+    private ILoginManager login_manager;
 }
--- MvvmTemplate/ViewModels/SettingsTab/IAccountSettingsPageViewModel.cs
+++ MvvmTemplate/ViewModels/SettingsTab/IAccountSettingsPageViewModel.cs
@@ -1,8 +1,14 @@
 using System.ComponentModel;
 
+using Reactive.Bindings;
+
 namespace MvvmTemplate.ViewModels;
 
-public interface IAccountSettingsPageViewModel : INotifyPropertyChanged
+public interface IAccountSettingsPageViewModel : INotifyPropertyChanged, IDisposable
 {
+    public ReactiveProperty<string> Username { get; }
 
+    public ReactiveProperty<string> Password { get; }
+
+    public ReactiveProperty<string> ConfirmPassword { get; }
 }
